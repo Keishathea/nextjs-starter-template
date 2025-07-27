b70a57e68e28f7924415be2ed86607e047edeab3
Keishathea <samsonmelitante2025@gmail.com>
2025-07-27 15:48:12 +0800

    Add .next/static/chunks/src_app_vendor-suggestions_page_tsx_a993527f._.js 1753602492236

---
+++ .next/static/chunks/src_app_vendor-suggestions_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_b7563120._.js",
+  "static/chunks/node_modules_a2d3e0b2._.js"
+],
+    source: "dynamic"
+});
